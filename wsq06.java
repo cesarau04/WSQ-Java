ca0585a5f68e6574c664b7ca4bcf1a8df7ce5132
César Augusto <cesarau.04@gmail.com>
2016-02-13 12:50:55 -0600

    Create wsq06.java

---
+++ wsq06.java
@@ -0,0 +1,31 @@
+//WSQ06 Cesar Augusto Garcia Perez  A01153737
+//WSQ06 Cesar Augusto Cortez 		A01226862
+//WSQ06 Jose Andres Choza Castro	A01113487
+class GCD{
+	private int n = 2; 
+	private int result = 1;
+	
+	public GCD(){
+	}
+	
+	public int isDivisible(int x, int y){
+		while (n < x){
+			if (x%n==0 && y%n==0){
+				x = x/n;
+				y = y/n;
+				result = n*result;
+				isDivisible(x,y);
+			}else{
+				n++;
+				isDivisible(x,y);
+				}
+		}
+		return result;
+	}
+		
+
+	public static void main(String[] args){
+		GCD myNumber = new GCD();
+		System.out.println("The GCD is: " + myNumber.isDivisible(60, 98));
+	}
+}
