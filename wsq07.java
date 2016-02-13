2f9961430c2c4bef0ae78e31beb2c8ea9449de61
César Augusto <cesarau.04@gmail.com>
2016-02-13 12:53:06 -0600

    Update wsq07.java

--- wsq07.java
+++ wsq07.java
@@ -1,6 +1,6 @@
 //WSQ06 Cesar Augusto Garcia Perez  A01153737
-//WSQ06 Cesar Augusto Cortez 		    A01226862
-//WSQ06 Jose Andres Choza Castro	  A01113487
+//WSQ06 Cesar Augusto Cortez 	    A01226862
+//WSQ06 Jose Andres Choza Castro    A01113487
 
 class BabyM{
 	double multiNum; double numPow =1.0; double preRoot; double root;
